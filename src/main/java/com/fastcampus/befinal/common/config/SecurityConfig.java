894c05aa2092b6c2e8cc22e99db579548d2ea5f9
GOren98 <gangmin9812@gmail.com>
2024-08-12 16:13:10 +0900

    setting : swagger 관련 api security 인증 제외

--- src/main/java/com/fastcampus/befinal/common/config/SecurityConfig.java
+++ src/main/java/com/fastcampus/befinal/common/config/SecurityConfig.java
@@ -3,16 +3,21 @@ package com.fastcampus.befinal.common.config;
 import com.fastcampus.befinal.common.filter.JwtAuthenticationFilter;
 import com.fastcampus.befinal.domain.service.JwtService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -21,6 +26,19 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 public class SecurityConfig {
     private final JwtService jwtService;
     private final AccessDeniedHandler accessDeniedHandler;
+
+    private final List<String> SWAGGER = List.of(
+            "/swagger-ui.html",
+            "/swagger-ui/**",
+            "/v3/api-docs/**"
+    );
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer(){
+        return (web)-> web.ignoring()
+            .requestMatchers(PathRequest.toStaticResources().atCommonLocations())
+            .requestMatchers(SWAGGER.toArray(new String[0]));
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -31,7 +49,7 @@ public class SecurityConfig {
             .requestMatchers(HttpMethod.POST, "/api/v1/auth/login").permitAll()
             .requestMatchers(HttpMethod.POST,"/api/v1/auth/logout").authenticated());
 
-        http.addFilterBefore(new JwtAuthenticationFilter(jwtService), BasicAuthenticationFilter.class);
+        http.addFilterBefore(new JwtAuthenticationFilter(jwtService), UsernamePasswordAuthenticationFilter.class);
 
         http.exceptionHandling(configurer -> configurer
             .accessDeniedHandler(accessDeniedHandler));
