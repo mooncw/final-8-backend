b8ea4fd8ec186958dffaae51bf913e9a0c6432e3
mooncw <dusrktl4@gmail.com>
2024-08-14 11:40:30 +0900

    setting: entity 세팅
    - User 엔티티 추가

--- src/main/java/com/fastcampus/befinal/domain/entity/User.java
+++ src/main/java/com/fastcampus/befinal/domain/entity/User.java
@@ -1,27 +1,58 @@
 package com.fastcampus.befinal.domain.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import lombok.NoArgsConstructor;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.time.LocalDateTime;
 
 @Getter
+@Entity(name = "User")
+@Table(name = "user")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
+    @Id
     private String id;
 
+    @Column(nullable = false, name = "name", columnDefinition = "varchar(10)")
+    private String name;
+
+    @Column(nullable = false, name = "password", columnDefinition = "varchar(50)")
     private String password;
 
-    private List<SimpleGrantedAuthority> authorities;
+    @Column(nullable = false, unique = true, name = "phone_number", columnDefinition = "varchar(11)")
+    private String phoneNumber;
+
+    @Column(nullable = false, unique = true, name = "emp_number", columnDefinition = "varchar(50)")
+    private String empNumber;
+
+    @Column(nullable = false, unique = true, name = "email", columnDefinition = "varchar(30)")
+    private String email;
+
+    @Column(nullable = false, name = "signup_datetime", columnDefinition = "datetime")
+    private LocalDateTime signUpDateTime;
+
+    @Column(nullable = false, name = "final_login_datetime", columnDefinition = "datetime")
+    private LocalDateTime finalLoginDateTime;
+
+    @Column(nullable = false, name = "role", columnDefinition = "varchar(20)")
+    private String role;
 
     @Builder
-    public User(String id, String password) {
+    public User(String id, String name, String password, String phoneNumber, String empNumber, String email, LocalDateTime signUpDateTime, LocalDateTime finalLoginDateTime, String role) {
         this.id = id;
+        this.name = name;
         this.password = password;
-        this.authorities = new ArrayList<>();
-        this.authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+        this.phoneNumber = phoneNumber;
+        this.empNumber = empNumber;
+        this.email = email;
+        this.signUpDateTime = signUpDateTime;
+        this.finalLoginDateTime = finalLoginDateTime;
+        this.role = "USER";
     }
-
-
 }
--- src/main/java/com/fastcampus/befinal/domain/info/UserDetailsInfo.java
+++ src/main/java/com/fastcampus/befinal/domain/info/UserDetailsInfo.java
@@ -3,9 +3,12 @@ package com.fastcampus.befinal.domain.info;
 import com.fastcampus.befinal.domain.entity.User;
 import lombok.Builder;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 @Builder
 public class UserDetailsInfo implements UserDetails {
@@ -13,7 +16,9 @@ public class UserDetailsInfo implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return user.getAuthorities();
+        List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority("ROLE_" + user.getRole()));
+        return authorities;
     }
 
     @Override
