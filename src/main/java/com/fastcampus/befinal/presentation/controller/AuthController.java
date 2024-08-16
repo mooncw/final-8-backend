cdbeab7d057e460dfff7c2bd86c0a964d3e5acc8
mooncw <dusrktl4@gmail.com>
2024-08-16 23:22:12 +0900

    refactor: AuthController swagger 로직 추가

--- src/main/java/com/fastcampus/befinal/presentation/controller/AuthController.java
+++ src/main/java/com/fastcampus/befinal/presentation/controller/AuthController.java
@@ -4,6 +4,8 @@ import com.fastcampus.befinal.application.facade.AuthFacade;
 import com.fastcampus.befinal.common.response.ApiResponse;
 import com.fastcampus.befinal.common.response.ResponseEntityFactory;
 import com.fastcampus.befinal.presentation.dto.AuthDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,9 +18,11 @@ import static com.fastcampus.befinal.common.response.success.info.JwtSuccessCode
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
+@Tag(name = "Auth", description = "인증 관련 API")
 public class AuthController {
     private final AuthFacade authFacade;
 
+    @Operation(summary = "JWT 토큰 재발급")
     @PostMapping("/reissue")
     public ResponseEntity<ApiResponse<AuthDto.ReissueJwtResponse>> reissueAccessToken(
         @RequestBody
