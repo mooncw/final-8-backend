0f31ac4a15365e5d60b3d21d5f440ed2c9673e30
mooncw <dusrktl4@gmail.com>
2024-08-21 02:05:27 +0900

    feat: 회원가입시 비밀번호를 암호화하여 저장하기 위한 Encoder 추가
    - SecurityConfig에서 PasswordEncoder로 BCryptPasswordEncoder를 사용

--- src/main/java/com/fastcampus/befinal/common/config/SecurityConfig.java
+++ src/main/java/com/fastcampus/befinal/common/config/SecurityConfig.java
@@ -12,6 +12,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
@@ -37,6 +39,11 @@ public class SecurityConfig {
         return (web)-> web.ignoring()
             .requestMatchers(PathRequest.toStaticResources().atCommonLocations())
             .requestMatchers(SWAGGER.toArray(new String[0]));
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
