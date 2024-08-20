2cd29403840e63981c7a820f1758afa8b6dff580
mooncw <dusrktl4@gmail.com>
2024-08-21 00:46:56 +0900

    test: 회원가입 request Dto 테스트 작성
    - 각 검증 어노테이션에 검증 그룹 배정해서 각 그룹별 검증 테스트를 진행하게끔 구현

--- src/main/java/com/fastcampus/befinal/presentation/dto/AuthDto.java
+++ src/main/java/com/fastcampus/befinal/presentation/dto/AuthDto.java
@@ -1,6 +1,7 @@
 package com.fastcampus.befinal.presentation.dto;
 
 import com.fastcampus.befinal.common.annotation.ComplexPattern;
+import com.fastcampus.befinal.common.util.RequestValidationGroups;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -11,35 +12,36 @@ import lombok.Builder;
 import static com.fastcampus.befinal.common.contant.AuthConstant.*;
 
 public class AuthDto {
+    @Builder
     public record SignUpRequest(
-        @NotBlank(message = NOT_BLANK_USER_NAME)
-        @Size(min = 2, max = 4, message = SIZE_MISMATCH_USER_NAME)
-        @Pattern(regexp = "^[가-힣]+$", message = PATTERN_MISMATCH_USER_NAME)
+        @NotBlank(message = NOT_BLANK_USER_NAME, groups = RequestValidationGroups.NotBlankGroup.class)
+        @Size(min = 2, max = 4, message = SIZE_MISMATCH_USER_NAME, groups = RequestValidationGroups.SizeGroup.class)
+        @Pattern(regexp = "^[가-힣]+$", message = PATTERN_MISMATCH_USER_NAME, groups = RequestValidationGroups.PatternGroup.class)
         String name,
 
-        @NotBlank(message = NOT_BLANK_PHONE_NUMBER)
-        @Size(min = 11, max = 11, message = SIZE_MISMATCH_PHONE_NUMBER)
-        @Pattern(regexp = "^\\d+$", message = PATTERN_MISMATCH_PHONE_NUMBER)
+        @NotBlank(message = NOT_BLANK_PHONE_NUMBER, groups = RequestValidationGroups.NotBlankGroup.class)
+        @Size(min = 11, max = 11, message = SIZE_MISMATCH_PHONE_NUMBER, groups = RequestValidationGroups.SizeGroup.class)
+        @Pattern(regexp = "^\\d+$", message = PATTERN_MISMATCH_PHONE_NUMBER, groups = RequestValidationGroups.PatternGroup.class)
         String phoneNumber,
 
-        @NotBlank(message = NOT_BLANK_USER_ID)
-        @Size(min = 4, max = 12, message = SIZE_MISMATCH_USER_ID)
-        @Pattern(regexp = "^[a-zA-Z]+$", message = PATTERN_MISMATCH_USER_ID)
+        @NotBlank(message = NOT_BLANK_USER_ID, groups = RequestValidationGroups.NotBlankGroup.class)
+        @Size(min = 4, max = 12, message = SIZE_MISMATCH_USER_ID, groups = RequestValidationGroups.SizeGroup.class)
+        @Pattern(regexp = "^[a-zA-Z]+$", message = PATTERN_MISMATCH_USER_ID, groups = RequestValidationGroups.PatternGroup.class)
         String id,
 
-        @NotBlank(message = NOT_BLANK_USER_PASSWORD)
-        @Size(min = 8, max = 16, message = SIZE_MISMATCH_USER_PASSWORD)
+        @NotBlank(message = NOT_BLANK_USER_PASSWORD, groups = RequestValidationGroups.NotBlankGroup.class)
+        @Size(min = 8, max = 16, message = SIZE_MISMATCH_USER_PASSWORD, groups = RequestValidationGroups.SizeGroup.class)
         @ComplexPattern(patterns = { ".*[a-zA-Z].*", ".*\\d.*", ".*[!\"#$%&'()*+,-./:;<=>?@\\[\\]^_`{|}~].*" },
-            minMatches = 2, message = PATTERN_MISMATCH_USER_PASSWORD)
+            minMatches = 2, message = PATTERN_MISMATCH_USER_PASSWORD, groups = RequestValidationGroups.PatternGroup.class)
         String password,
 
-        @NotBlank(message = NOT_BLANK_USER_EMP_NOMBER)
-        @Size(min = 8, max = 8, message = SIZE_MISMATCH_USER_EMP_NOMBER)
-        @Pattern(regexp = "^\\d+$", message = PATTERN_MISMATCH_USER_EMP_NOMBER)
+        @NotBlank(message = NOT_BLANK_USER_EMP_NOMBER, groups = RequestValidationGroups.NotBlankGroup.class)
+        @Size(min = 8, max = 8, message = SIZE_MISMATCH_USER_EMP_NOMBER, groups = RequestValidationGroups.SizeGroup.class)
+        @Pattern(regexp = "^\\d+$", message = PATTERN_MISMATCH_USER_EMP_NOMBER, groups = RequestValidationGroups.PatternGroup.class)
         String empNo,
 
-        @NotBlank(message = NOT_BLANK_USER_EMAIL)
-        @Email(message = INVALID_FORMAT_USER_EMAIL)
+        @NotBlank(message = NOT_BLANK_USER_EMAIL, groups = RequestValidationGroups.NotBlankGroup.class)
+        @Email(message = INVALID_FORMAT_USER_EMAIL, groups = RequestValidationGroups.PatternGroup.class)
         String email
     ) {}
 
@@ -51,11 +53,11 @@ public class AuthDto {
     @Builder
     @Schema(description = "JWT 재발급 request")
     public record ReissueJwtRequest(
-        @NotBlank(message = NOT_BLANK_ACCESSTOKEN)
+        @NotBlank(message = NOT_BLANK_ACCESSTOKEN, groups = RequestValidationGroups.NotBlankGroup.class)
         @Schema(description = "예시 accessToken 형태", example = SWAGGER_REISSUE_REQUEST_ACCESSTOKEN)
         String accessToken,
 
-        @NotBlank(message = NOT_BLANK_REFRESHTOKEN)
+        @NotBlank(message = NOT_BLANK_REFRESHTOKEN, groups = RequestValidationGroups.NotBlankGroup.class)
         @Schema(description = "예시 refreshToken 형태", example = SWAGGER_REISSUE_REQUEST_REFRESHTOKEN)
         String refreshToken
     ) {}
