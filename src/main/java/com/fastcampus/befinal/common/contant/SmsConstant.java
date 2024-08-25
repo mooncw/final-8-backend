d2f7f1e9ce6981bdf5e0b33191a7e6d25d06472c
mooncw <dusrktl4@gmail.com>
2024-08-26 00:36:33 +0900

    feat: 인증 메시지를 보내는 SmsSenderImpl 구현

---
+++ src/main/java/com/fastcampus/befinal/common/contant/SmsConstant.java
@@ -0,0 +1,6 @@
+package com.fastcampus.befinal.common.contant;
+
+public class SmsConstant {
+    public static final int CERTIFICATION_NUMBER_LENGTH = 6;
+    public static final String CERTIFICATION_SENDER_NAME = "NeuroFlow";
+}
