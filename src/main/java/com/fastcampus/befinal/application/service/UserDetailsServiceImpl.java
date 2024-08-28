780ccaff47138365228248d19652fc79ca19219c
mooncw <dusrktl4@gmail.com>
2024-08-28 21:33:19 +0900

    refactor: 인증 유저에 대한 정보를 가져오는 loadUserByUsername 메서드에 Transactional 어노테이션 추가

--- src/main/java/com/fastcampus/befinal/application/service/UserDetailsServiceImpl.java
+++ src/main/java/com/fastcampus/befinal/application/service/UserDetailsServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -15,6 +16,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     private final UserReader userReader;
 
     @Override
+    @Transactional(readOnly = true)
     public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
         User user = userReader.findUser(userId);
         return UserDetailsInfo.from(user);
