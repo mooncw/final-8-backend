33d1b1773e72003c5eeec851bb7c4477a0488fa4
mooncw <dusrktl4@gmail.com>
2024-08-29 01:34:48 +0900

    feat: 회원가입 승인 Service 구현

---
+++ src/main/java/com/fastcampus/befinal/application/service/AdminServiceImpl.java
@@ -0,0 +1,31 @@
+package com.fastcampus.befinal.application.service;
+
+import com.fastcampus.befinal.domain.command.AdminCommand;
+import com.fastcampus.befinal.domain.dataprovider.UserManagementReader;
+import com.fastcampus.befinal.domain.dataprovider.UserManagementStore;
+import com.fastcampus.befinal.domain.dataprovider.UserStore;
+import com.fastcampus.befinal.domain.entity.UserManagement;
+import com.fastcampus.befinal.domain.service.AdminService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class AdminServiceImpl implements AdminService {
+    private final UserManagementReader userManagementReader;
+    private final UserManagementStore userManagementStore;
+    private final UserStore userStore;
+
+    @Override
+    @Transactional
+    public void approveUser(AdminCommand.ApproveUserRequest command) {
+        command.userList().stream()
+            .map(AdminCommand.ApproveUser::empNo)
+            .forEach(empNo -> {
+                UserManagement userManagement = userManagementReader.findByEmpNo(empNo);
+                userStore.store(userManagement);
+                userManagementStore.delete(userManagement);
+            });
+    }
+}
