6d9ce3e21681a97415314ab3fa0cc89010e9792a
Hyeonjoon <kingpark02@naver.com>
2024-08-30 02:39:01 +0900

    feat: 대시보드 Controller에 swagger 설정 추가

--- src/main/java/com/fastcampus/befinal/presentation/controller/UserBoardController.java
+++ src/main/java/com/fastcampus/befinal/presentation/controller/UserBoardController.java
@@ -7,6 +7,10 @@ import com.fastcampus.befinal.common.response.error.exception.BusinessException;
 import com.fastcampus.befinal.common.response.error.info.DashBoardErrorCode;
 import com.fastcampus.befinal.common.response.success.info.DashBoardSuccessCode;
 import com.fastcampus.befinal.presentation.dto.DashBoardDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -23,9 +27,41 @@ public class UserBoardController {
     private final BoardFacade boardFacade;
 
     @GetMapping
-    public ResponseEntity<AppApiResponse<DashBoardDto.DashBoardDataResponse>> getBoardData (
-
-    ) {
+    @Operation(summary = "사용자 대시보드")
+    @ApiResponse(responseCode = "200", description = "대시보드 확인 가능합니다.",
+            content = @Content(
+                mediaType = "application/json",
+                schema = @Schema(
+                    implementation = DashBoardDto.DashBoardDataResponse.class,
+                    example = "{\n" +
+                            "  \"code\": 1200,\n" +
+                            "  \"message\": \"대시보드 확인 가능합니다.\",\n" +
+                            "  \"data\": {\n" +
+                            "    \"totalAd\": 4,\n" +
+                            "    \"myAd\": 3,\n" +
+                            "    \"totalDoneAd\": 3,\n" +
+                            "    \"myDoneAd\": 3,\n" +
+                            "    \"totalNotDoneAd\": 1,\n" +
+                            "    \"myNotDoneAd\": 0,\n" +
+                            "    \"dailyDoneList\": [\n" +
+                            "      {\n" +
+                            "        \"date\": \"2024-08-25\",\n" +
+                            "        \"dailyMyDoneAd\": 2\n" +
+                            "      }\n" +
+                            "    ],\n" +
+                            "    \"recentDoneList\": [\n" +
+                            "      {\n" +
+                            "        \"adId\": \"a3\",\n" +
+                            "        \"adName\": \"toy\",\n" +
+                            "        \"adModifiedDate\": \"2024-08-27T09:00:00\"\n" +
+                            "      }\n" +
+                            "    ]\n" +
+                            "  }\n" +
+                            "}"
+                    )
+            )
+    )
+    public ResponseEntity<AppApiResponse<DashBoardDto.DashBoardDataResponse>> getBoardData() {
         String userId = "testID";
         if(userId == null) {
             throw new BusinessException(DashBoardErrorCode.DENIED_ACCESS_DASHBOARD);
