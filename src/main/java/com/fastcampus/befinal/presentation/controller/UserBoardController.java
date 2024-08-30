066fc0661529815f709549226d69a0a861175cc0
Hyeonjoon <kingpark02@naver.com>
2024-08-30 22:00:28 +0900

    refactor: 사용자 대시보드 Controller 수정 - swagger 설정 변경 - @AuthenticationPrincipal로 User 정보 사용

--- src/main/java/com/fastcampus/befinal/presentation/controller/UserBoardController.java
+++ src/main/java/com/fastcampus/befinal/presentation/controller/UserBoardController.java
@@ -3,10 +3,9 @@ package com.fastcampus.befinal.presentation.controller;
 import com.fastcampus.befinal.application.facade.BoardFacade;
 import com.fastcampus.befinal.common.response.AppApiResponse;
 import com.fastcampus.befinal.common.response.ResponseEntityFactory;
-import com.fastcampus.befinal.common.response.error.exception.BusinessException;
-import com.fastcampus.befinal.common.response.error.info.DashBoardErrorCode;
-import com.fastcampus.befinal.common.response.success.info.DashBoardSuccessCode;
-import com.fastcampus.befinal.presentation.dto.DashBoardDto;
+import com.fastcampus.befinal.common.response.success.info.DashboardSuccessCode;
+import com.fastcampus.befinal.domain.entity.User;
+import com.fastcampus.befinal.presentation.dto.DashboardDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -14,6 +13,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/dashboard")
 @RequiredArgsConstructor
-@Tag(name = "DashBoard", description = "사용자 대시보드 관련 API")
+@Tag(name = "Dashboard", description = "사용자 대시보드 관련 API")
 public class UserBoardController {
 
     private final BoardFacade boardFacade;
@@ -32,42 +32,41 @@ public class UserBoardController {
             content = @Content(
                 mediaType = "application/json",
                 schema = @Schema(
-                    implementation = DashBoardDto.DashBoardDataResponse.class,
-                    example = "{\n" +
-                            "  \"code\": 1200,\n" +
-                            "  \"message\": \"대시보드 확인 가능합니다.\",\n" +
-                            "  \"data\": {\n" +
-                            "    \"totalAd\": 4,\n" +
-                            "    \"myAd\": 3,\n" +
-                            "    \"totalDoneAd\": 3,\n" +
-                            "    \"myDoneAd\": 3,\n" +
-                            "    \"totalNotDoneAd\": 1,\n" +
-                            "    \"myNotDoneAd\": 0,\n" +
-                            "    \"dailyDoneList\": [\n" +
-                            "      {\n" +
-                            "        \"date\": \"2024-08-25\",\n" +
-                            "        \"dailyMyDoneAd\": 2\n" +
-                            "      }\n" +
-                            "    ],\n" +
-                            "    \"recentDoneList\": [\n" +
-                            "      {\n" +
-                            "        \"adId\": \"a3\",\n" +
-                            "        \"adName\": \"toy\",\n" +
-                            "        \"adModifiedDate\": \"2024-08-27T09:00:00\"\n" +
-                            "      }\n" +
-                            "    ]\n" +
-                            "  }\n" +
+                    implementation = DashboardDto.DashboardDataResponse.class,
+                    example = "{" +
+                            "\"code\": 1200, " +
+                            "\"message\": \"대시보드 확인 가능합니다.\", " +
+                            "\"data\": {" +
+                            "   \"adCount\": {" +
+                            "      \"totalAd\": 4," +
+                            "      \"myAd\": 3," +
+                            "      \"totalDoneAd\": 3," +
+                            "      \"myDoneAd\": 3," +
+                            "      \"totalNotDoneAd\": 1," +
+                            "      \"myNotDoneAd\": 0," +
+                            "      \"dailyDoneList\": [" +
+                            "      {" +
+                            "        \"date\": \"2024-08-25," +
+                            "        \"dailyMyDoneAd\": 2" +
+                            "      }" +
+                            "    ]," +
+                            "    \"recentDoneList\": [" +
+                            "      {" +
+                            "        \"adId\": a3" +
+                            "        \"adName\": toy" +
+                            "        \"adTaskDateTime\": 2024-08-27T09:00:00" +
+                            "      }" +
+                            "    ]" +
+                            "  }" +
                             "}"
                     )
             )
     )
-    public ResponseEntity<AppApiResponse<DashBoardDto.DashBoardDataResponse>> getBoardData() {
-        String userId = "testID";
-        if(userId == null) {
-            throw new BusinessException(DashBoardErrorCode.DENIED_ACCESS_DASHBOARD);
-        }
-
-        DashBoardDto.DashBoardDataResponse response = boardFacade.loadUserDashBoardData(userId);
-        return ResponseEntityFactory.toResponseEntity(DashBoardSuccessCode.CHECK_DASHBOARD_SUCCESS, response);
+    public ResponseEntity<AppApiResponse<DashboardDto.DashboardDataResponse>> getBoardData(
+            @AuthenticationPrincipal User user
+    ) {
+        String userId = user.getId();
+        DashboardDto.DashboardDataResponse response = boardFacade.loadUserDashboardData(userId);
+        return ResponseEntityFactory.toResponseEntity(DashboardSuccessCode.CHECK_DASHBOARD_SUCCESS, response);
     }
 }
