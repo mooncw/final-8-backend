209dd8dcaea3a5afe7174df60ffe00186ed13623
Hyeonjoon <kingpark02@naver.com>
2024-09-03 02:18:22 +0900

    refactor: LocalDateTime에서 ZonedDateTime 변경 메서드 Mapper로 이동 - DashboardDto.RecentDone의 adTaskDateTime의 타입을 ZonedDateTime으로 변경 - toZoneDateTime 메서드를 DashboardDtoMapper에 생성

--- src/main/java/com/fastcampus/befinal/application/mapper/DashboardDtoMapper.java
+++ src/main/java/com/fastcampus/befinal/application/mapper/DashboardDtoMapper.java
@@ -2,9 +2,11 @@ package com.fastcampus.befinal.application.mapper;
 
 import com.fastcampus.befinal.domain.info.DashboardInfo;
 import com.fastcampus.befinal.presentation.dto.DashboardDto;
-import org.mapstruct.InjectionStrategy;
-import org.mapstruct.Mapper;
-import org.mapstruct.ReportingPolicy;
+import org.mapstruct.*;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 @Mapper(
         componentModel = "spring",
@@ -15,6 +17,12 @@ public interface DashboardDtoMapper {
     DashboardDto.DashboardDataResponse from(DashboardInfo.DashboardDataInfo dataInfo);
     DashboardDto.AdCount from(DashboardInfo.AdCount adCount);
     DashboardDto.DailyDone from(DashboardInfo.DailyDone dailyDone);
+
+    @Mapping(source = "adTaskDateTime", target = "adTaskDateTime", qualifiedByName = "toZonedDateTime")
     DashboardDto.RecentDone from(DashboardInfo.RecentDone recentDone);
 
+    @Named("toZonedDateTime")
+    default ZonedDateTime toZonedDateTime(LocalDateTime adTaskDateTime) {
+        return adTaskDateTime.atZone(ZoneId.of("Asia/Seoul"));
+    }
 }
--- src/main/java/com/fastcampus/befinal/presentation/dto/DashboardDto.java
+++ src/main/java/com/fastcampus/befinal/presentation/dto/DashboardDto.java
@@ -1,12 +1,9 @@
 package com.fastcampus.befinal.presentation.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
+import java.time.ZonedDateTime;
 import java.util.List;
 
 public class DashboardDto {
@@ -37,13 +34,6 @@ public class DashboardDto {
     public record RecentDone(
             String adId,
             String adName,
-            LocalDateTime adTaskDateTime
-    ) {
-        @JsonProperty("adTaskDateTime")
-        public String getAdTaskDateTimeAsString() {
-            return adTaskDateTime
-                    .atZone(ZoneId.of("Asia/Seoul"))
-                    .format(DateTimeFormatter.ISO_OFFSET_DATE_TIME);
-        }
-    }
+            ZonedDateTime adTaskDateTime
+    ) {}
 }
