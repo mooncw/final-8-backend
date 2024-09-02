589aaa333015518400f2f844b93839bc93858a86
GOren98 <gangmin9812@gmail.com>
2024-09-03 00:44:36 +0900

    refactor : 로컬에서 쓰던 security config 제거

--- src/main/java/com/fastcampus/befinal/common/config/SecurityConfig.java
+++ src/main/java/com/fastcampus/befinal/common/config/SecurityConfig.java
@@ -64,9 +64,7 @@ public class SecurityConfig {
             .requestMatchers("/api/v1/admin/**").hasAuthority(ADMIN_AUTHORITY)
             .requestMatchers(HttpMethod.PUT, "/api/v1/user/**").authenticated()
             .requestMatchers(HttpMethod.GET, "/api/health-check").permitAll()
-            .requestMatchers(HttpMethod.GET, "/actuator/**").permitAll()
-            .requestMatchers("/api/test/signIn").permitAll()
-            .requestMatchers("/test/test").authenticated());
+            .requestMatchers(HttpMethod.GET, "/actuator/**").permitAll());
 
         http.addFilterBefore(new JwtAuthenticationFilter(jwtAuthService), BasicAuthenticationFilter.class);
 
