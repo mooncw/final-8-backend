1b1ecca0f40de3a850fdaf3d26bcff850cbc6487
mooncw <dusrktl4@gmail.com>
2024-09-04 02:36:56 +0900

    refactor: UserUnionView 변경에 따른 코드 수정

--- src/main/java/com/fastcampus/befinal/domain/repository/UserUnionViewRepository.java
+++ src/main/java/com/fastcampus/befinal/domain/repository/UserUnionViewRepository.java
@@ -4,7 +4,9 @@ import com.fastcampus.befinal.domain.entity.UserUnionView;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserUnionViewRepository extends JpaRepository<UserUnionView, String> {
-    boolean existsByIdOrPhoneNumberOrEmpNumberOrEmail(String id, String phoneNumber, String empNumber, String email);
+    boolean existsByUserId(String userId);
+
+    boolean existsByUserIdOrPhoneNumberOrEmpNumberOrEmail(String id, String phoneNumber, String empNumber, String email);
 
     boolean existsByPhoneNumber(String phoneNumber);
 }
