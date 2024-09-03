ee028cb2ebb29df0574433203ee72c6e5b499c13
mooncw <dusrktl4@gmail.com>
2024-09-04 02:52:22 +0900

    test: 회원 정보 관리 조회 기능 test 코드 구현
    - User 엔티티 변경에 따른 연관된 test 코드 수정도 포함

--- src/test/java/com/fastcampus/befinal/application/service/AdminServiceImplTest.java
+++ src/test/java/com/fastcampus/befinal/application/service/AdminServiceImplTest.java
@@ -1,10 +1,13 @@
 package com.fastcampus.befinal.application.service;
 
+import com.fastcampus.befinal.common.util.ScrollPagination;
 import com.fastcampus.befinal.domain.command.AdminCommand;
 import com.fastcampus.befinal.domain.dataprovider.UserManagementReader;
 import com.fastcampus.befinal.domain.dataprovider.UserManagementStore;
+import com.fastcampus.befinal.domain.dataprovider.UserReader;
 import com.fastcampus.befinal.domain.dataprovider.UserStore;
 import com.fastcampus.befinal.domain.entity.UserManagement;
+import com.fastcampus.befinal.domain.info.AdminInfo;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -15,6 +18,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import static com.fastcampus.befinal.common.contant.AuthConstant.USER_AUTHORITY;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -29,6 +34,9 @@ public class AdminServiceImplTest {
 
     @Mock
     private UserManagementStore userManagementStore;
+
+    @Mock
+    private UserReader userReader;
 
     @Mock
     private UserStore userStore;
@@ -46,7 +54,7 @@ public class AdminServiceImplTest {
             .build();
 
         UserManagement userManagement = UserManagement.builder()
-            .id("aaaa")
+            .userId("aaaa")
             .name("홍길동")
             .password("aaaaaaa1")
             .phoneNumber("01011112222")
@@ -75,5 +83,38 @@ public class AdminServiceImplTest {
         verify(userManagementReader, times(1)).findByEmpNo(anyString());
         verify(userStore, times(1)).store(any(UserManagement.class));
         verify(userManagementStore, times(1)).delete(any(UserManagement.class));
+    }
+
+    @Test
+    @DisplayName("회원 정보 목록 조회 성공 테스트")
+    void findUserScrollTest() {
+        //given
+        Long cursorId = 2L;
+
+        AdminInfo.UserInfo info = AdminInfo.UserInfo.builder()
+            .id(cursorId)
+            .empNumber("11111111")
+            .name("홍길동")
+            .role(USER_AUTHORITY)
+            .userId("hong")
+            .phoneNumber("01011112222")
+            .email("hong@hong.com")
+            .signUpDateTime(LocalDateTime.now().minusDays(5L))
+            .finalLoginDateTime(LocalDateTime.now().minusDays(2L))
+            .build();
+
+        ScrollPagination<Long, AdminInfo.UserInfo> doReturnScroll = ScrollPagination.of(4L, cursorId, List.of(info));
+
+        doReturn(doReturnScroll)
+            .when(userReader)
+            .findScroll(anyLong());
+
+        //when
+        ScrollPagination<Long, AdminInfo.UserInfo> scroll = adminService.findUserScroll(cursorId);
+
+        //then
+        assertThat(scroll.totalElements()).isEqualTo(doReturnScroll.totalElements());
+        assertThat(scroll.currentCursorId()).isEqualTo(doReturnScroll.currentCursorId());
+        assertThat(scroll.contents()).isEqualTo(doReturnScroll.contents());
     }
 }
