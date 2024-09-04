402a33efccb25b246e59b28a84f1344be0133b91
mooncw <dusrktl4@gmail.com>
2024-09-04 20:02:14 +0900

    bugfix: 빈문자열도 통과 못하게 UserUpdateValidator 검증 강화

--- src/test/java/com/fastcampus/befinal/presentation/dto/UserDtoTest.java
+++ src/test/java/com/fastcampus/befinal/presentation/dto/UserDtoTest.java
@@ -26,7 +26,7 @@ public class UserDtoTest {
         //given
         UserDto.UserUpdateRequest request = UserDto.UserUpdateRequest.builder()
             .phoneNumber(null)
-            .email(null)
+            .email("")
             .certNoCheckToken(null)
             .build();
 
