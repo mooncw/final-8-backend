8fb4a2648121b8477ba4c954bdf89a1df950258b
Hyeonjoon <kingpark02@naver.com>
2024-09-06 03:15:38 +0900

    feat: 나의 작업 성공 Code, Constant 생성

---
+++ src/main/java/com/fastcampus/befinal/common/contant/TaskConstant.java
@@ -0,0 +1,9 @@
+package com.fastcampus.befinal.common.contant;
+
+public class TaskConstant {
+    // 나의 작업 조건
+    public static final String MIN_LENGTH_KEYWORD = "검색어를 2글자 이상 입력해주세요.";
+
+    // 나의 작업 스크롤 사이즈
+    public static final int MY_TASK_LIST_SCROLL_SIZE = 9;
+}
