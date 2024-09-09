4d3391f760968302aabf870bd7ceafa3c6b6b569
mooncw <dusrktl4@gmail.com>
2024-09-09 11:05:25 +0900

    bugfix: CorsConfig 변경
    - 프런트 포트번호 추가

--- src/main/java/com/fastcampus/befinal/common/config/CorsConfig.java
+++ src/main/java/com/fastcampus/befinal/common/config/CorsConfig.java
@@ -10,7 +10,8 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
             .allowedOriginPatterns("http://13.124.172.3:8880", "http://13.124.172.3:8881",
-                "http://localhost:8880", "http://localhost:8881")
+                "http://localhost:8880", "http://localhost:8881",
+                "http://13.124.172.3:5173", "http://localhost:5173")
             .allowedMethods("GET", "POST", "PUT", "DELETE")
             .allowedHeaders("*")
             .allowCredentials(true);
