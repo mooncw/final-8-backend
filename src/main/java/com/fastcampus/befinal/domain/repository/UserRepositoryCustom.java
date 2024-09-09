dadc700aa216f97f344e94e81adf6b99569506e9
mooncw <dusrktl4@gmail.com>
2024-09-10 03:30:44 +0900

    feat: 작업자 관리 정보 조회 QueryDsl 구현
    - offset 사용

--- src/main/java/com/fastcampus/befinal/domain/repository/UserRepositoryCustom.java
+++ src/main/java/com/fastcampus/befinal/domain/repository/UserRepositoryCustom.java
@@ -1,10 +1,17 @@
 package com.fastcampus.befinal.domain.repository;
 
+import com.fastcampus.befinal.common.response.error.exception.BusinessException;
 import com.fastcampus.befinal.common.util.ScrollPagination;
+import com.fastcampus.befinal.domain.command.AdminCommand;
+import com.fastcampus.befinal.domain.entity.QAdvertisement;
 import com.fastcampus.befinal.domain.entity.QUser;
 import com.fastcampus.befinal.domain.info.AdminInfo;
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.CaseBuilder;
+import com.querydsl.core.types.dsl.NumberExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -12,13 +19,16 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 import static com.fastcampus.befinal.common.contant.AuthConstant.ADMIN_AUTHORITY;
+import static com.fastcampus.befinal.common.contant.ScrollConstant.MANAGE_EMP_SCROLL_SIZE;
 import static com.fastcampus.befinal.common.contant.ScrollConstant.MANAGE_USER_SCROLL_SIZE;
+import static com.fastcampus.befinal.common.response.error.info.AdminErrorCode.INVALID_USER_TASK_SORT_TYPE;
 
 @Repository
 @RequiredArgsConstructor
 public class UserRepositoryCustom {
     private final JPAQueryFactory queryFactory;
     private static final QUser user = QUser.user;
+    private static final QAdvertisement ad = QAdvertisement.advertisement;
 
     // cursorId는 User EmpNumber와 동일합니다.
     public ScrollPagination<Long, AdminInfo.UserInfo> findScrollByEmpNumber(Long cursorId) {
@@ -66,5 +76,83 @@ public class UserRepositoryCustom {
             return lastUserInfo.id();
         }
         return cursorId;
+    }
+
+    public ScrollPagination<Integer, AdminInfo.UserTaskInfo> findScrollOrderByRequest(AdminCommand.FindUserTaskListRequest command) {
+        NumberExpression<Integer> totalAd = new CaseBuilder()
+            .when(user.id.eq(ad.assignee.id)).then(1)
+            .otherwise(0).sum();
+
+        NumberExpression<Integer> notDoneAd = new CaseBuilder()
+            .when(user.id.eq(ad.assignee.id).and(ad.state.eq(false))).then(1)
+            .otherwise(0).sum();
+
+        NumberExpression<Integer> doneAd = new CaseBuilder()
+            .when(user.id.eq(ad.assignee.id).and(ad.state.eq(true))).then(1)
+            .otherwise(0).sum();
+
+        NumberExpression<Double> doneRatio = new CaseBuilder()
+            .when(totalAd.eq(0)).then(0.0)
+            .otherwise(doneAd.divide(totalAd).doubleValue());
+
+        List<AdminInfo.UserTaskInfo> contents = queryFactory
+            .select(Projections.constructor(AdminInfo.UserTaskInfo.class,
+                user.id,
+                user.empNumber,
+                user.name,
+                totalAd,
+                notDoneAd,
+                doneAd,
+                doneRatio
+            ))
+            .from(user)
+            .leftJoin(ad)
+            .on(user.id.eq(ad.assignee.id))
+            .groupBy(user.id)
+            .orderBy(orderCase(command, doneAd, doneRatio).toArray(new OrderSpecifier[0]))
+            .limit(MANAGE_EMP_SCROLL_SIZE)
+            .offset((command.cursorId() - 1) * MANAGE_EMP_SCROLL_SIZE)
+            .fetch();
+
+        Long totalElements = queryFactory
+            .select(user.count())
+            .from(user)
+            .fetchOne();
+
+        return ScrollPagination.of(totalElements, command.cursorId(), contents);
+    }
+
+    private List<OrderSpecifier<?>> orderCase(AdminCommand.FindUserTaskListRequest command,
+                                              NumberExpression<Integer> doneAd, NumberExpression<Double> doneRatio) {
+        switch (command.sorted()) {
+            case EMP_NUMBER -> {
+                return List.of(new OrderSpecifier<>(Order.ASC, user.empNumber));
+            }
+            case DONE_DESC -> {
+                return List.of(
+                    new OrderSpecifier<>(Order.DESC, doneAd),
+                    new OrderSpecifier<>(Order.ASC, user.empNumber)
+                );
+            }
+            case DONE_ASC -> {
+                return List.of(
+                    new OrderSpecifier<>(Order.ASC, doneAd),
+                    new OrderSpecifier<>(Order.ASC, user.empNumber)
+                );
+            }
+            case DONE_RATIO_DESC -> {
+                return List.of(
+                    new OrderSpecifier<>(Order.DESC, doneRatio),
+                    new OrderSpecifier<>(Order.ASC, user.empNumber)
+                );
+            }
+            case DONE_RATIO_ASC -> {
+                return List.of(
+                    new OrderSpecifier<>(Order.ASC, doneRatio),
+                    new OrderSpecifier<>(Order.ASC, user.empNumber)
+                );
+            }
+            default -> throw new BusinessException(INVALID_USER_TASK_SORT_TYPE);
+        }
     }
 }
