04d31ba2d3c984f91bb91faf72a1b2f01d4b3fff
Hyeonjoon <kingpark02@naver.com>
2024-09-10 11:33:06 +0900

    feat: Filter Controller, Service 구현

--- src/main/java/com/fastcampus/befinal/presentation/controller/FilterController.java
+++ src/main/java/com/fastcampus/befinal/presentation/controller/FilterController.java
@@ -6,6 +6,9 @@ import com.fastcampus.befinal.common.response.ResponseEntityFactory;
 import com.fastcampus.befinal.common.response.success.info.FilterSuccessCode;
 import com.fastcampus.befinal.presentation.dto.FilterDto;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -24,18 +27,60 @@ public class FilterController {
     private final FilterFacade filterFacade;
 
     @GetMapping("/media")
-    @Operation(summary = "매체명 필터 옵션 검색", description = "키워드에 따른 매체명 필터 옵션 검색합니다.")
+    @Operation(summary = "필터 매체명 리스트 조회")
+    @ApiResponse(responseCode = "200", description = "필터 매체명 리스트 조회되었습니다.",
+        content = @Content(
+            mediaType = "application/json",
+            schema = @Schema(
+                example = "{ " +
+                    "\"code\": 3600, " +
+                    "\"message\": \"필터 매체명 리스트 조회되었습니다.\", " +
+                    "\"data\": [" +
+                    "{ " +
+                    "\"name\": \"동아일보\", " +
+                    "\"adCount\": 8 " +
+                    "}, " +
+                    "{ " +
+                    "\"name\": \"조선일보\", " +
+                    "\"adCount\": 6 " +
+                    "}" +
+                    "]" +
+                    "}"
+            )
+        )
+    )
     public ResponseEntity<AppApiResponse<List<FilterDto.FilterOptionResponse>>> searchMediaOptions(
-            @RequestParam(required = false) String keyword
+        @RequestParam(required = false) String keyword
     ) {
         List<FilterDto.FilterOptionResponse> response = filterFacade.searchMediaOptions(keyword);
         return ResponseEntityFactory.toResponseEntity(FilterSuccessCode.FILTER_MEDIA_LIST, response);
     }
 
     @GetMapping("/category")
-    @Operation(summary = "업종명 필터 옵션 검색", description = "키워드에 따른 업종명 필터 옵션 검색합니다.")
+    @Operation(summary = "필터 업종명 리스트 조회")
+    @ApiResponse(responseCode = "200", description = "필터 업종명 리스트 조회되었습니다.",
+        content = @Content(
+            mediaType = "application/json",
+            schema = @Schema(
+                example = "{ " +
+                    "\"code\": 3601, " +
+                    "\"message\": \"필터 업종명 리스트 조회되었습니다.\", " +
+                    "\"data\": [" +
+                    "{ " +
+                    "\"name\": \"가정용품\", " +
+                    "\"adCount\": 8 " +
+                    "}, " +
+                    "{ " +
+                    "\"name\": \"의료\", " +
+                    "\"adCount\": 6 " +
+                    "}" +
+                    "]" +
+                    "}"
+            )
+        )
+    )
     public ResponseEntity<AppApiResponse<List<FilterDto.FilterOptionResponse>>> searchCategoryOptions(
-            @RequestParam(required = false) String keyword
+        @RequestParam(required = false) String keyword
     ) {
         List<FilterDto.FilterOptionResponse> response = filterFacade.searchCategoryOptions(keyword);
         return ResponseEntityFactory.toResponseEntity(FilterSuccessCode.FILTER_CATEGORY_LIST, response);
