22512c6cf420a832e1fcda01c55cac40a2f4bcba
mooncw <dusrktl4@gmail.com>
2024-09-11 18:51:24 +0900

    test: 작업 배분 작업자 목록 test 코드 구현

--- src/test/java/com/fastcampus/befinal/presentation/controller/AdminControllerTest.java
+++ src/test/java/com/fastcampus/befinal/presentation/controller/AdminControllerTest.java
@@ -256,4 +256,20 @@ public class AdminControllerTest {
             .andExpect(jsonPath("code").value(FIND_UNASSIGNED_AD_LIST_SUCCESS.getCode()))
             .andExpect(jsonPath("message").value(FIND_UNASSIGNED_AD_LIST_SUCCESS.getMessage()));
     }
+
+    @Test
+    @WithMockUser(authorities = ADMIN_AUTHORITY)
+    @DisplayName("작업 배분 작업자 목록 조회 성공시, 200 OK와 정상 응답을 반환")
+    void findAssigneeListTest() throws Exception {
+        //when
+        ResultActions perform = mockMvc.perform(get("/api/v1/admin/manage-task/emp")
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON)
+            .characterEncoding(StandardCharsets.UTF_8));
+
+        //then
+        perform.andExpect(status().is(FIND_ASSIGNEE_LIST_SUCCESS.getHttpStatus().value()))
+            .andExpect(jsonPath("code").value(FIND_ASSIGNEE_LIST_SUCCESS.getCode()))
+            .andExpect(jsonPath("message").value(FIND_ASSIGNEE_LIST_SUCCESS.getMessage()));
+    }
 }
