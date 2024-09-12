4c5bb721d77d7d15038d43b96a67f523405ca889
Hyeonjoon <kingpark02@naver.com>
2024-09-13 00:43:41 +0900

    feat: CheckTokenReader에 find 메서드 추가 - 비밀번호 초기화 토큰을 통해 userId 응답

--- src/main/java/com/fastcampus/befinal/domain/dataprovider/CheckTokenReader.java
+++ src/main/java/com/fastcampus/befinal/domain/dataprovider/CheckTokenReader.java
@@ -4,4 +4,6 @@ import com.fastcampus.befinal.domain.info.AuthInfo;
 
 public interface CheckTokenReader {
     boolean exists(AuthInfo.CheckTokenInfo info);
+
+    String find(AuthInfo.CheckTokenInfo info);
 }
--- src/main/java/com/fastcampus/befinal/infrastructure/redis/dataprovider/CheckTokenReaderImpl.java
+++ src/main/java/com/fastcampus/befinal/infrastructure/redis/dataprovider/CheckTokenReaderImpl.java
@@ -1,11 +1,18 @@
 package com.fastcampus.befinal.infrastructure.redis.dataprovider;
 
 import com.fastcampus.befinal.common.annotation.DataProvider;
+import com.fastcampus.befinal.common.response.error.exception.BusinessException;
 import com.fastcampus.befinal.domain.dataprovider.CheckTokenReader;
 import com.fastcampus.befinal.domain.entity.RedisValue;
+import com.fastcampus.befinal.domain.entity.UserId;
 import com.fastcampus.befinal.domain.info.AuthInfo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
+
+import java.util.Optional;
+import static com.fastcampus.befinal.common.response.error.info.AuthErrorCode.INVALID_PASSWORD_RESET_TOKEN;
+import static com.fastcampus.befinal.common.response.error.info.AuthErrorCode.NOT_FOUND_USER;
+
 
 @DataProvider
 @RequiredArgsConstructor
@@ -15,5 +22,16 @@ public class CheckTokenReaderImpl implements CheckTokenReader {
     @Override
     public boolean exists(AuthInfo.CheckTokenInfo info) {
         return Boolean.TRUE.equals(redisTemplate.hasKey(info.token()));
+    }
+
+    @Override
+    public String find(AuthInfo.CheckTokenInfo info) {
+        RedisValue value = Optional.ofNullable(redisTemplate.opsForValue().get(info.token()))
+            .orElseThrow(() -> new BusinessException(INVALID_PASSWORD_RESET_TOKEN));
+
+        if(value instanceof UserId) {
+            return ((UserId)value).getUserId();
+        }
+        throw new BusinessException(NOT_FOUND_USER);
     }
 }
