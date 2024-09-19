79616eb30814b6e5676581b35258e6a68f620988
GOren98 <gangmin9812@gmail.com>
2024-09-19 18:09:48 +0900

    test : 지적광고 리스트 조회 테스트코드

--- src/test/java/com/fastcampus/befinal/application/service/IssueAdServiceImplTest.java
+++ src/test/java/com/fastcampus/befinal/application/service/IssueAdServiceImplTest.java
@@ -1,8 +1,11 @@
 package com.fastcampus.befinal.application.service;
 
+import com.fastcampus.befinal.common.util.ScrollPagination;
+import com.fastcampus.befinal.domain.command.TaskCommand;
 import com.fastcampus.befinal.domain.dataprovider.AdReviewReader;
 import com.fastcampus.befinal.domain.dataprovider.AdvertisementReader;
 import com.fastcampus.befinal.domain.info.IssueAdInfo;
+import com.fastcampus.befinal.domain.info.TaskInfo;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,6 +15,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.*;
 
 @DisplayName("IssueAdService 테스트")
@@ -25,6 +30,70 @@ public class IssueAdServiceImplTest {
 
     @Mock
     private AdReviewReader adReviewReader;
+
+    @Test
+    @DisplayName("지적광고 조회 성공 테스트")
+    void findIssueAdListTest() {
+        //given
+        TaskCommand.FilterConditionRequest command = TaskCommand.FilterConditionRequest.builder()
+            .cursorInfo(TaskCommand.CursorInfo.builder()
+                .cursorState(false)
+                .cursorId("A0001")
+                .build())
+            .state(false)
+            .issue(false)
+            .media(List.of("온라인"))
+            .category(List.of("IT"))
+            .build();
+
+        TaskInfo.AdvertisementListInfo adInfo = TaskInfo.AdvertisementListInfo.builder()
+            .adId("A0001")
+            .media("온라인")
+            .category("IT")
+            .product("노트북")
+            .advertiser("테크컴퍼니")
+            .state(false)
+            .issue(false)
+            .build();
+
+        List<TaskInfo.AdvertisementListInfo> adList = List.of(adInfo);
+
+        ScrollPagination<TaskInfo.CursorInfo, TaskInfo.AdvertisementListInfo> filterTaskPagination =
+            ScrollPagination.of(
+                10L,
+                TaskInfo.CursorInfo.builder()
+                    .cursorState(false)
+                    .cursorId("A0001")
+                    .build(),
+                adList
+            );
+
+        doReturn(filterTaskPagination)
+            .when(advertisementReader)
+            .findIssueAdList(command);
+
+        //when
+        TaskInfo.TaskListInfo request = issueAdService.findIssueAdList(command);
+
+        //then
+        assertNotNull(request);
+
+        assertEquals(filterTaskPagination.totalElements(), request.totalElements());
+        assertEquals(filterTaskPagination.currentCursorId().cursorState(), request.cursorInfo().cursorState());
+        assertEquals(filterTaskPagination.currentCursorId().cursorId(), request.cursorInfo().cursorId());
+
+        assertEquals(1, request.advertisementList().size());
+        TaskInfo.AdvertisementListInfo firstAd = request.advertisementList().getFirst();
+        assertEquals("A0001", firstAd.adId());
+        assertEquals("온라인", firstAd.media());
+        assertEquals("IT", firstAd.category());
+        assertEquals("노트북", firstAd.product());
+        assertEquals("테크컴퍼니", firstAd.advertiser());
+        assertEquals(false, firstAd.state());
+        assertEquals(false, firstAd.issue());
+
+        verify(advertisementReader, times(1)).findIssueAdList(command);
+    }
 
     @Test
     @DisplayName("지적광고 상세조회 성공 테스트")
--- src/test/java/com/fastcampus/befinal/presentation/controller/IssueAdControllerTest.java
+++ src/test/java/com/fastcampus/befinal/presentation/controller/IssueAdControllerTest.java
@@ -5,6 +5,7 @@ import com.fastcampus.befinal.common.config.SecurityConfig;
 import com.fastcampus.befinal.domain.info.IssueAdInfo;
 import com.fastcampus.befinal.domain.service.JwtAuthService;
 import com.fastcampus.befinal.presentation.dto.IssueAdDto;
+import com.fastcampus.befinal.presentation.dto.TaskDto;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -25,10 +26,12 @@ import java.util.List;
 import static com.fastcampus.befinal.common.contant.AuthConstant.USER_AUTHORITY;
 
 import static com.fastcampus.befinal.common.response.success.info.IssueAdSuccessCode.GET_ADVERTISEMENT_DETAIL_SUCCESS;
-import static org.mockito.ArgumentMatchers.anyString;
+import static com.fastcampus.befinal.common.response.success.info.IssueAdSuccessCode.GET_ISSUE_ADVERTISEMENT_LIST_SUCCESS;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doReturn;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -50,6 +53,53 @@ class IssueAdControllerTest {
 
     @Autowired
     private ObjectMapper objectMapper;
+
+    @Test
+    @WithMockUser(authorities = USER_AUTHORITY)
+    @DisplayName("지적광고 조회 요청 시, 200 OK 및 정상 응답을 반환")
+    void findIssueAdListTest() throws Exception{
+        //given
+        TaskDto.AdvertisementListInfo adResponse = TaskDto.AdvertisementListInfo.builder()
+            .adId("A00001")
+            .media("동아일보")
+            .category("음식")
+            .product("상품명")
+            .advertiser("광고주")
+            .state(true)
+            .issue(true)
+            .build();
+
+        TaskDto.TaskListInfo response = TaskDto.TaskListInfo.builder()
+            .totalElements(1L)
+            .cursorInfo(new TaskDto.CursorInfo(true, "A00001"))
+            .advertisementList(List.of(adResponse))
+            .build();
+
+        doReturn(response)
+            .when(issueAdFacade)
+            .findIssueAdList(any(TaskDto.FilterConditionRequest.class));
+
+        //when
+        TaskDto.FilterConditionRequest firstRequest = TaskDto.FilterConditionRequest.builder()
+            .media(List.of("동아일보"))
+            .state(true)
+            .build();
+
+        ResultActions firstPerform = mockMvc.perform(post("/api/v1/issue-ad")
+            .with(csrf())
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(firstRequest))
+            .accept(MediaType.APPLICATION_JSON));
+
+        // then
+        firstPerform.andExpect(status().is(GET_ISSUE_ADVERTISEMENT_LIST_SUCCESS.getHttpStatus().value()))
+            .andExpect(jsonPath("code").value(GET_ISSUE_ADVERTISEMENT_LIST_SUCCESS.getCode()))
+            .andExpect(jsonPath("message").value(GET_ISSUE_ADVERTISEMENT_LIST_SUCCESS.getMessage()))
+            .andExpect(jsonPath("$.data.advertisementList[0].media").value("동아일보"))
+            .andExpect(jsonPath("$.data.advertisementList[0].adId").value("A00001"))
+            .andExpect(jsonPath("$.data.advertisementList[0].state").value(true));
+
+    }
 
     @Test
     @WithMockUser(authorities = USER_AUTHORITY)
