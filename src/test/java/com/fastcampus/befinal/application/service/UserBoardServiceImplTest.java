00358e37dbf4b39db2f6a6f2173d3cc92933950e
GOren98 <gangmin9812@gmail.com>
2024-09-21 23:18:44 +0900

    test : 관리자 대시보드 테스트 코드 작성

--- src/test/java/com/fastcampus/befinal/application/service/UserBoardServiceImplTest.java
+++ src/test/java/com/fastcampus/befinal/application/service/UserBoardServiceImplTest.java
@@ -1,6 +1,7 @@
 package com.fastcampus.befinal.application.service;
 
 import com.fastcampus.befinal.domain.dataprovider.AdvertisementReader;
+import com.fastcampus.befinal.domain.dataprovider.UserManagementReader;
 import com.fastcampus.befinal.domain.info.DashboardInfo;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -24,6 +25,9 @@ class UserBoardServiceImplTest {
 
     @Mock
     private AdvertisementReader advertisementReader;
+
+    @Mock
+    private UserManagementReader userManagementReader;
 
     @Test
     @DisplayName("대시보드 성공 테스트")
@@ -65,5 +69,57 @@ class UserBoardServiceImplTest {
         verify(advertisementReader, times(1)).findAdCount(userId);
         verify(advertisementReader, times(1)).findDailyDone(userId);
         verify(advertisementReader, times(1)).findRecentDone(userId);
+    }
+
+    @Test
+    @DisplayName("관리자 대시보드 성공 테스트")
+    void loadAdminDashBoardTest() {
+        // given
+        Integer notApprovedUser = 1;
+
+        DashboardInfo.AdminAdCountInfo adCount = new DashboardInfo.AdminAdCountInfo(1,1, 1, 0);
+        List<DashboardInfo.TodayWork> todayWorkList = new ArrayList<>();
+        List<DashboardInfo.DailyAvgDone> dailyAvgDoneList = new ArrayList<>();
+        List<DashboardInfo.PersonalTask> personalTaskList = new ArrayList<>();
+
+        doReturn(notApprovedUser)
+            .when(userManagementReader)
+            .findNotApproveUserCount();
+
+        doReturn(adCount)
+            .when(advertisementReader)
+            .findAdminAdCountInfo();
+
+        doReturn(todayWorkList)
+            .when(advertisementReader)
+            .findTodayWorkList();
+
+        doReturn(dailyAvgDoneList)
+            .when(advertisementReader)
+            .findDailyAvgDoneList();
+
+        doReturn(personalTaskList)
+            .when(advertisementReader)
+            .findPersonalTaskList();
+
+        // when
+        DashboardInfo.DashboardAdminDataInfo result = userBoardService.loadAdminDashboardData();
+
+        // then
+        assertNotNull(result);
+        assertEquals(notApprovedUser, result.adminTimeline().notApprovedUser());
+        assertEquals(adCount.remainingAd(), result.adminTimeline().remainingAd());
+        assertEquals(adCount.totalAd(), result.adCount().totalAd());
+        assertEquals(adCount.doneAd(), result.adCount().doneAd());
+        assertEquals(adCount.notDoneAd(), result.adCount().notDoneAd());
+        assertEquals(todayWorkList, result.todayWorkList());
+        assertEquals(dailyAvgDoneList, result.dailyAvgDoneList());
+        assertEquals(personalTaskList, result.personalTaskList());
+
+        verify(userManagementReader, times(1)).findNotApproveUserCount();
+        verify(advertisementReader, times(1)).findAdminAdCountInfo();
+        verify(advertisementReader, times(1)).findTodayWorkList();
+        verify(advertisementReader, times(1)).findDailyAvgDoneList();
+        verify(advertisementReader, times(1)).findPersonalTaskList();
     }
 }
